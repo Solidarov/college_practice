4a050807c38c48a14884e38a9a9ec048ecb1c48b
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2025-06-21 12:29:08 +0300

    Complete second task with arithmetic average of negative number

--- chapter2/array_lib.cpp
+++ chapter2/array_lib.cpp
@@ -4,6 +4,7 @@
 #include <limits>
 #include <type_traits>
 #include <map>
+#include <random>
 
 #include "array_lib.hpp"
 using namespace std;
@@ -30,6 +31,24 @@ void minElement(int arr[], int size){
     cout << "Minimum value is (" << minValue << ") at serial number (" << minSerNum << ")"<< endl;
 }
 
+void avgNegArithm(double mtx[4][3], int m, int n){
+    double avg = 0, count = 0;
+    for (int i = 0; i < 4 && i < m; i++){
+        for (int j = 0; j < 3 && j < n; j++){
+            if (mtx[i][j] < 0){
+                avg += mtx[i][j];
+                count++;
+            }
+        }
+    }
+    if (!(avg == 0 && count == 0)){
+        avg = round((avg / count) * 100) / 100;
+    }
+    cout << "\nArithmetic Average of Negative number is: " << avg << endl;
+
+}
+
+
 void genAutoArray(int arr[], int& size){
     srand(time(0));
     for (int i = 0; i < size; i++){
@@ -53,6 +72,31 @@ void genManArray(int arr[], int size){
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         i++;
         size--;
+    }
+}
+
+void genAutoMatrix(double mtx[4][3], int m, int n){
+    double number;
+    uniform_real_distribution<double> unif(-100, 100);
+    default_random_engine re(time(0));
+
+    for (int i = 0; i < m && i < 4; i++){
+        for (int j = 0; j < n && j < 3; j++){
+            number = round(unif(re) * 100) / 100;
+            mtx[i][j] = number;
+        }
+    }
+}
+
+void genManMatrix(double mtx[4][3], int m, int n){
+    double number; 
+    for (int i = 0; i < m && i < 4; i++){
+        cout << i + 1 << " row: ";
+        for (int j = 0; j < n && j < 3; j++){
+            getUntilNum(number, "Enter value: ");
+            mtx[i][j] = round(number * 100) / 100;
+        }
+        cout << "\n";
     }
 }
 
@@ -87,8 +131,23 @@ void getUserChoice(int& choice, map<int, string> av_options){
     }
 }
 
-int getUntilNum(string msg){
-    int dInput;
+void getUntilNum(int& iInput, string msg){
+    cout << "\n" << msg;
+    cin >> iInput;
+
+    while (!cin){
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Invalid input. \n" << msg;
+        cin >> iInput;
+    }
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+}
+
+void getUntilNum(double& dInput, string msg){
     cout << "\n" << msg;
     cin >> dInput;
 
@@ -102,7 +161,6 @@ int getUntilNum(string msg){
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    return dInput;
 }
 
 void printOutArray(int arr[], int size){
@@ -111,4 +169,15 @@ void printOutArray(int arr[], int size){
         cout << arr[i] << ", ";
     }
     cout << "]" << endl;
+}
+
+void printOutMatrix(double mtx[4][3], int m, int n){
+    cout << "Content of the matrix:" << endl;
+    for (int i = 0; i < m && i < 4; i++){
+        cout << "\t";
+        for (int j = 0; j < n && j < 3; j++){
+            cout << mtx[i][j] << "\t\t";
+        }
+        cout << endl;
+    } 
 }
--- chapter2/array_lib.hpp
+++ chapter2/array_lib.hpp
@@ -9,6 +9,11 @@
 void minElement(int arr[], int size);
 
 /*
+    Обчислює середнє арифметичне значення від'ємних елементів
+*/
+void avgNegArithm(double mtx[4][3], int m, int n);
+
+/*
     Автоматична генерація масиву в діапазоні від 0 до 100
 */
 void genAutoArray(int arr[], int& size);
@@ -19,6 +24,16 @@ void genAutoArray(int arr[], int& size);
 void genManArray(int arr[], int size);
 
 /*
+    Автоматично генерує матрицю 4*3 в діапазоні [-100; 100] 
+*/
+void genAutoMatrix(double mtx[4][3], int m, int n);
+
+/*
+    Користувач вручну генерує матрицю 4*3
+*/
+void genManMatrix(double mtx[4][3], int m, int n);
+
+/*
     Перевіряє правильність вибору користувача із запропонованих варіантів
 */
 void getUserChoice(int& choice, std::map<int, std::string> av_options);
@@ -26,11 +41,21 @@ void getUserChoice(int& choice, std::map<int, std::string> av_options);
 /*
     Перевіряє правильний користувацький ввід для типу int
 */
-int getUntilNum(std::string msg);
+void getUntilNum(int& iInput, std::string msg);
+
+/*
+    Перевіряє правильний користувацький ввід для типу double  
+*/
+void getUntilNum(double& dInput, std::string msg);
 
 /*
     Виводить вміст масиву на екран
 */
 void printOutArray(int arr[], int size);
+
+/*
+    Виводить вміст матриці на екран
+*/
+void printOutMatrix(double mtx[4][3], int m, int n);
 
 #endif
